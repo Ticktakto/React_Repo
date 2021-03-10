2771e16a4c7828f40886fed54bd4ff57afb49006
정다운 <daunny@jeongdaun-ui-MacBookPro.local>
2021-03-10 15:33:14 +0900

    Initize project and add props

---
+++ schedule/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import MyComponent from './myComponent';
+
+const App = () => {
+  return <MyComponent name="INSERT" Number={15}> Your React </MyComponent>;
+};
+
+export default App;
