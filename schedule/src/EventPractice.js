d392b3e7a2af01ea89afa161ee3c9d68f54c4bf3
정다운 <daunny@jeongdaun-ui-MacBookPro.local>
2021-03-10 19:58:15 +0900

    change handling event && add onKeyPress

--- schedule/src/EventPractice.js
+++ schedule/src/EventPractice.js
@@ -3,34 +3,54 @@ import React, { Component } from 'react';
 class EventPractice extends Component {
 
     state = {
+        username : '',
         message : " "
     }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value // e.target.name => key값!
+            // (중요) event 객체 활용 -> 여러 개 input 처리 가능!
+        });
+    }
+
+    handleClick = () => {
+        alert(this.state.username + ": " + this.state.message);
+        this.setState({
+            username: '',
+            message: '' // 다시 초기화 
+        });
+    }
+
+    handleKeyPress = (e) => {
+        if(e.key === 'Enter') {
+            this.handleClick();
+        }
+    }
+
     render() {
         return (
             <div>
                 <h1>Practice Event!</h1>
                 <input
                    type="text"
+                   name="username"
+                   placeholder="User-Name"
+                   value= {this.state.username}
+                   onChange={this.handleChange}
+                
+                    />
+                <input
+                   type="text"
                    name="message"
                    placeholder="Do Texting in here!"
                    value= {this.state.message}
-                   onChange={
-                       (e) => {
-                           this.setState({
-                               message : e.target.value
-                           })
-                       }
-                   }
+                   onChange={this.handleChange}
+                   onKeyPress={this.handleKeyPress}
                 
                     />
-
                 <button
-                   onClick={() => {
-                       alert(this.state.message);
-                       this.setState({
-                           message: '' // 다시 초기화
-                       });
-                   }}
+                   onClick={this.handleClick}
                 >확인</button>    
             </div>
         );
