8461ea33ea929ab22ed8db33954714bb12d9a619
정다운 <wjdek88@naver.com>
2021-05-05 21:00:10 +0900

    Implement Adding Data (2/3)

--- schedule/src/IterationSample.js
+++ schedule/src/IterationSample.js
@@ -8,9 +8,29 @@ const IterationSample = () => {
     {id: 4, text: '바람'}
     ]);
     const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5); // 새로운 항목 추가 시 사용할 id
+
+    const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        // concat => array 이어주는 내장함수(새로운 배열을 생성하여)
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+
+        setNextId(nextId + 1); // id update (+1)
+        setNames(nextNames); // names update (name add)
+        setInputText(''); // inputText 비우기
+    };
 
     const nameList = names.map( name => <li key={name.id}>{name.text}</li>);
-    return <ul>{nameList}</ul>;
+    return (
+    <>
+    <input value={inputText} onChange={onChange}/>
+    <button onClick={onClick}>추가</button>
+    <ul>{nameList}</ul>
+    </>
+    );
 }
 
 export default IterationSample;
