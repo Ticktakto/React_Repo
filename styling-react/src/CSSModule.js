6c101a8e9ffff27bb58e83a18077b83defe124cb
정다운 <wjdek88@naver.com>
2021-05-20 16:29:47 +0900

    Practice CSS & SASS - Update ClassNames Module in CSSModule.js (3/4)

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './CSSModule.module.css';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles); // 미리 styles에서 클래스를 받아 오도록 설정
 
 const CSSModule = () => {
    return(
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
+    <div className={cx('wrapper', 'inverted')}>
     안녕하세요, 저는 <span className="something">CSS MOdule!</span>
     </div>
    );
