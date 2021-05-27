4b692a82f9554294b30fae609cf4fbc2cefa8629
정다운 <wjdek88@naver.com>
2021-05-27 20:33:03 +0900

    Practice Router Module - Add Sub Router System on App.js & Profiles.js

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 const App = () => {
   return(
@@ -15,15 +15,13 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/velopert">velopert profile</Link>
+          <Link to="/profiles">profile</Link>
         </li>
-        <li>
-          <Link to="/profile/gildong">gildong profile</Link>
-        </li>
+       
       </ul>
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   )
 }
