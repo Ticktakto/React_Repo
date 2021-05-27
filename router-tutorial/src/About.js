5207bb8ee2748ef4a68e35c2e0e11737353ba304
정다운 <wjdek88@naver.com>
2021-05-27 20:21:51 +0900

    Practice Router Module - Add URL query using qs module on About.js

--- router-tutorial/src/About.js
+++ router-tutorial/src/About.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({location}) => {
+    const query = qs.parse(location.search, {
+        ignoreQueryPrefix: true // 이 설정은 문자열 맨 앞의 "?"를 생략함
+    });
+    const showDetail = query.detail === 'true'; // 쿼리의 파싱 결과 값은 문자열
     return (
 
         <div>
             <h1>소개</h1>
             <p>이 프로젝트는 리액트 라우터 기초를 실습하는 예제 프로젝트 입니다.</p>
+            {showDetail && <p>detail 값을 true로 설정했습니다.</p>}
         </div>
     );
 };
