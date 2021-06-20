26050c0a21bf99645d0c3f2ea8210c84502a3aba
DaunJeong <wjdek88@naver.com>
2021-06-20 15:22:32 +0900

    Practice_Context - Add Updating States by Clicking color button using Context API

---
+++ context-tutorial/src/components/SelectColors.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ColorConsumer } from '../contexts/color';
+const color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const SelectColors = () => {
+    return(
+        <div>
+            <h2>색상을 선택하세요.</h2>
+            <ColorConsumer>
+            {( {actions }) => (
+                <div style={{ display: 'flex' }}>
+                    {color.map(color => (
+                        <div
+                         key={color}
+                         style={{ background: color, width: '24px', height: '24px', cursor : 'pointer' }}
+                         onClick={()=> actions.setColor(color)}
+                         onContextMenu={e => {
+                            e.preventDefault(); // 마우스 오른쪽 버튼 클릭 시 메뉴 뜨는 것 무시
+                            actions.setSubcolor(color);
+                         }}
+                         />
+                    ))}
+                </div>
+            )}
+            </ColorConsumer>
+            <hr />
+        </div>
+    );
+}
+
+export default SelectColors;
