8a31298f00a45604b9766c41229f5aff5a1057ab
DaunJeong <wjdek88@naver.com>
2021-06-20 14:28:41 +0900

    Practice_Context - Add Context Proivder

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ColorBox from './components/colorBox';
+import ColorContext from './contexts/color';
 
 const App = () => {
   return(
-    <div>
-    <ColorBox />
-    </div>
+   <ColorContext.Provider value={{ color: 'red'}}>
+     <div>
+       <ColorBox />
+     </div>
+   </ColorContext.Provider>
   );
 }
 export default App;
