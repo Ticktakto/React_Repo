cac88a688a54370335e43aeaef58635d5763ac06
DaunJeong <wjdek88@naver.com>
2021-06-20 16:29:16 +0900

    Vanilla Reducx - Add Reducer, Store, Render

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
@@ -15,3 +17,46 @@ const initialState = {
     toggle : false,
     counter: 0
 };
+
+// Reducer 구현, state -> undefined 일 경우, initialState 가 기본값
+function reducer( state = initialState, action) {
+    switch (action.type) {
+        case TOGGLE_SWITCH:
+            return {
+                ...state, // 불변성 유지
+                toggle: !state.toggle
+            };
+        
+        case INCREASE:
+            return {
+                ...state,
+                counter: state.counter + action.difference
+            };
+        
+        case DECREASE:
+            return {
+                ...state,
+                counter: state.counter - 1
+            };
+        default:
+            return state;
+    }
+};
+
+// Store
+const store = createStore(reducer);
+
+// Render, 실제로 Html에 표시하는 함수 -> 이를 store 바뀔 때 마다 수행(redux.subscribe 이용)
+const render = () => {
+    const state = store.getState();
+    // process toggle
+    if( state.toggle ) {
+        divToggle.classList.add('active');
+    } else {
+        divToggle.classList.remove('active');
+    }
+    // process counter
+    counter.innerText = state.counter;
+};
+
+render();
