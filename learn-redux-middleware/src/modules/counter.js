38a1c366c9702ef8086272ec066d03eb778c2521
DaunJeong <wjdek88@naver.com>
2021-06-29 19:13:00 +0900

    Practice redux-middleware - Add redux-thunk

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -6,6 +6,14 @@ const DECREASE = 'counter/DECREASE';
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
+// 1초 뒤 increase or decrease 함수를 디스패치
+export const increaseAsync = () => dispatch => {
+    setTimeout(() => { dispatch(increase()) }, 1000);
+}
+export const decreaseAsync = () => dispatch => {
+    setTimeout(() => { dispatch(decrease()) }, 1000);
+};
+
 const initialState = 0; // 상태는 객체 뿐만 아니라, 숫자도 허용
 
 const counter = handleActions(
