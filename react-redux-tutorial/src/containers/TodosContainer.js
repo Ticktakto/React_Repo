7bf7c8b793533ef69dd12d7f46746bf5eb8cb543
DaunJeong <wjdek88@naver.com>
2021-06-29 18:17:36 +0900

    Practice react-redux - Same processing on todosContainer.js

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,38 +1,30 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
 
-const TodosContainer = ({
-    input,
-    todos,
-    changeInput,
-    insert,
-    toggle,
-    remove,
-}) => {
-    return (
-        <Todos
-        input={input}
-        todos={todos}
-        onChangeInput={changeInput}
-        onInsert={insert}
-        onToggle={toggle}
-        onRemove={remove}
-        />
+const TodosContainer = () => {
+    const { input, todos } = useSelector( ({ todos }) => ({
+        input: todos.input,
+        todos: todos.todos
+    }));
+const dispatch = useDispatch();
+
+const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch] );
+const onInsert = useCallback(text => dispatch(insert(text)), [dispatch] );
+const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch] );
+const onRemove = useCallback(id => dispatch(remove(id)), [dispatch] );
+
+return (
+    <Todos
+    input={input}
+    todos={todos}
+    onChangeInput={onChangeInput}
+    onInsert={onInsert}
+    onToggle={onToggle}
+    onRemove={onRemove}
+    />
     );
 };
 
-export default connect(
-    // 비구조화 할당 -> todos 분리 & state.todos.input 대신 todos.input 이용
-    ({ todos }) => ({
-        input: todos.input,
-        todos: todos.todos,
-    }) ,
-    {   // 받은 함수를 객체화 처리 -> redux의 bindActionCreator 함수가 처리!
-        changeInput,
-        insert,
-        toggle,
-        remove
-    }
-) (TodosContainer);
+export default TodosContainer;
