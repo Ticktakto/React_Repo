dd1ca941fd0a090a70472b200ca615a1f4bb3817
DaunJeong <wjdek88@naver.com>
2021-06-30 23:43:26 +0900

    Practice redux-middleware - Refactoring SampleContainer (Delete Repetition code)

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,7 +1,7 @@
 import { createAction, handleActions } from "redux-actions";
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga/effects';
 import * as api from '../lib/api';
-import { startLoading, finishLoading } from './loading';
+import createRequestSaga from "../lib/createRequestSaga";
 
 // 액션 타입 선언
 // 한 요청 당 세 개를 만들어야 함
@@ -18,45 +18,8 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 export const getPost = createAction(GET_POST, id => id);
 export const getUsers = createAction(GET_USERS);
 
-function* getPostSaga(action) {
-    yield put(startLoading(GET_POST)); // loading start
-    try{
-        // call을 사용하면 Promise를 반환하는 함수를 호출, 기다릴 수 있음
-        // 첫 번쨰 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인자
-        const post = yield call(api.getPost, action.payload); // const post = api.getPost(action.payload)
-        yield put({
-            type: GET_POST_SUCCESS,
-            payload: post.data
-        });
-    } catch (e) {
-        // can catch error, too.
-        yield put({
-            type: GET_POST_FAILURE,
-            payload: e,
-            error: true
-        });
-    }
-    yield put(finishLoading(GET_POST));
-}
-
-function* getUsersSaga() {
-    yield put(startLoading(GET_USERS));
-    try{
-        const users = yield call(api.getUsers);
-        yield put({
-            type: GET_USERS_SUCCESS,
-            payload: users.data
-        });
-    }
-    catch(e) {
-        yield put({
-            type: GET_USERS_FAILURE,
-            payload: e,
-            error: true
-        });
-    }
-    yield put(finishLoading(GET_USERS));
-}
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
 
 export function* sampleSaga() {
     yield takeLatest(GET_POST, getPostSaga);
