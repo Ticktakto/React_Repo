62daf01f9d21e3728ad1e2b2c7f22912e70f33bf
DaunJeong <wjdek88@naver.com>
2021-06-30 23:22:21 +0900

    Practice redux-middleware - Refactoring CounterContainer using Redux-Saga instead Redux-Thunk

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,4 +1,5 @@
 import {createAction, handleActions } from 'redux-actions';
+import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -6,15 +7,31 @@ const DECREASE = 'counter/DECREASE';
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
-// 1초 뒤 increase or decrease 함수를 디스패치
-export const increaseAsync = () => dispatch => {
-    setTimeout(() => { dispatch(increase()) }, 1000);
-}
-export const decreaseAsync = () => dispatch => {
-    setTimeout(() => { dispatch(decrease()) }, 1000);
-};
+const INCREASE_ASYNC = 'counter/INCREASE_ASYNC';
+const DECREASE_ASYNC = 'counter/DECREASE_ASYNC';
 
-const initialState = 0; // 상태는 객체 뿐만 아니라, 숫자도 허용
+// 마우스 클릭 이벤트가 payload로 들어가지 않도록, () => undefined를 두 번쨰 파라미터로 정함.
+export const increaseAsync = createAction(INCREASE_ASYNC, () => undefined );
+export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined );
+
+function* increaseSaga() {
+    yield delay(1000); // 1sec delay
+    yield put(increase()); // 특정 액션을 dispatch
+}
+
+function* decreaseSaga() {
+    yield delay(1000);
+    yield put(decrease());
+}
+
+export function* counterSaga() {
+    //takeEvery = 들어오는 모든 액션에 대해 특정 작업을 처리
+    yield takeEvery(INCREASE_ASYNC, increaseSaga);
+    // takeLastest = 기존에 진행 중이던 작업이 있다면 취소 처리 후, 가장 마지막으로 실행된 작업만 수행
+    yield takeLatest(DECREASE_ASYNC, decreaseSaga);
+}
+
+const initialState = 0;
 
 const counter = handleActions(
     {
