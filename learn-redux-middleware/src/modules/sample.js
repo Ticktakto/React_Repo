213ea28b180ffca7080c977014b64096a560c85e
DaunJeong <wjdek88@naver.com>
2021-06-30 23:35:08 +0900

    Practice redux-middleware - Refactoring SampleContainer using Redux-Saga instead Redux-Thunk

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,21 +1,67 @@
-import { handleActions } from "redux-actions";
+import { createAction, handleActions } from "redux-actions";
+import { call, put, takeLatest } from 'redux-saga/effects';
 import * as api from '../lib/api';
-import createRequestThunk from "../lib/createRequestThunk";
+import { startLoading, finishLoading } from './loading';
 
 // 액션 타입 선언
 // 한 요청 당 세 개를 만들어야 함
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
+const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
+const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
-// make Thunk function
-// thunk 함수는 내부에서 시작할 때, 성공했을 때, 실패했을 떄 각각 다른 액션을 디스패치
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
+export const getPost = createAction(GET_POST, id => id);
+export const getUsers = createAction(GET_USERS);
+
+function* getPostSaga(action) {
+    yield put(startLoading(GET_POST)); // loading start
+    try{
+        // call을 사용하면 Promise를 반환하는 함수를 호출, 기다릴 수 있음
+        // 첫 번쨰 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인자
+        const post = yield call(api.getPost, action.payload); // const post = api.getPost(action.payload)
+        yield put({
+            type: GET_POST_SUCCESS,
+            payload: post.data
+        });
+    } catch (e) {
+        // can catch error, too.
+        yield put({
+            type: GET_POST_FAILURE,
+            payload: e,
+            error: true
+        });
+    }
+    yield put(finishLoading(GET_POST));
+}
+
+function* getUsersSaga() {
+    yield put(startLoading(GET_USERS));
+    try{
+        const users = yield call(api.getUsers);
+        yield put({
+            type: GET_USERS_SUCCESS,
+            payload: users.data
+        });
+    }
+    catch(e) {
+        yield put({
+            type: GET_USERS_FAILURE,
+            payload: e,
+            error: true
+        });
+    }
+    yield put(finishLoading(GET_USERS));
+}
+
+export function* sampleSaga() {
+    yield takeLatest(GET_POST, getPostSaga);
+    yield takeLatest(GET_USERS, getUsersSaga);
+}
 // setting initial state
 // 요청의 로딩 중 상태는 loading 이라는 객체가 관리
 
