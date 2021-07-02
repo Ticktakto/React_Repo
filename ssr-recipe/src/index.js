15f7c33477b04e773108e268d424ab13ad30a3c3
DaunJeong <wjdek88@naver.com>
2021-07-02 14:47:39 +0900

    Server Side Rendering - Implementation Redux-Thunk code

--- ssr-recipe/src/index.js
+++ ssr-recipe/src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import  { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
+  <Provider store={store}>
   <BrowserRouter>
   <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
