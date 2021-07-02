f98250f25424f37f36a215a53476705a7287dc48
DaunJeong <wjdek88@naver.com>
2021-07-02 14:14:35 +0900

    Server Side Rendering - Implementation Processing Static Files on Server

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -1,8 +1,71 @@
 import React from 'react';
-import ReactDOMServer from 'react-dom/server'
+import ReactDOMServer from 'react-dom/server';
+import express from 'express';
+import { StaticRouter } from 'react-router-dom';
+import App from './App';
+import path from 'path';
+import fs from 'fs';
+import { func } from 'prop-types';
 
-const html = ReactDOMServer.renderToString(
-    <div>Hello Server Side Rendering</div>
+// asset-manifest.json에서 파일 경로들을 조회
+const manifest = JSON.parse(
+    fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8')
 );
 
-console.log(html);
+const chunks = Object.keys(manifest.files)
+    .filter(key => /chunk\.js$/.exec(key)) // (정규식인)chunk.js로 끝나는 키를 찾아서
+    .map(key => `<script src="${manifest.files[key]}"></script>`) // script 태그로 변환 후
+    .join(''); // 합치기
+
+function createPage(root) {
+    return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset=""utf-8 />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <meta
+                name="viewport"
+                content="width=device-width,initial-scale=1,shrink-to-fit=no"
+                />
+                <meta name="theme-color" content="#000000" />
+                <title>React App</title>
+                <link href="${manifest.files['main.css']}" rel="stylesheet" />
+                </head>
+                <body>
+                <noscript>You need to enable JavaScript to run this app.</noscript>
+                <div id="root">
+                    ${root}
+                </div>
+                <script src="${manifest.files['runtime-main.js']}"></script>
+                ${chunks}
+                <script src="${manifest.files['main.js']}"></script>
+                </body>
+                </html>
+    `;
+}
+
+const app = express();
+
+// Event Handler for processing server side rendering
+const serverRender = (req, res, next) => {
+    // 이 함수는 404 떠야하는 상황에서 404 대신 서버 사이드 렌더링을 해 줍니다.
+    const context = {};
+    const jsx = (
+        <StaticRouter location={req.url} context={context}>
+            <App />
+        </StaticRouter>
+    );
+    const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
+    res.send(createPage(root)); // client에게 결과물을 응답합니다.
+};
+
+const serve = express.static(path.resolve('./build'), {
+    index: false // "/" 경로에서 index.html을 보여주지 않도록 설정합니다.
+})
+
+app.use(serve); // JS 특성 상, 순서가 중요!!
+app.use(serverRender);
+
+app.listen(5000, () => {
+    console.log('Running on http://localhost:5000');
+})
